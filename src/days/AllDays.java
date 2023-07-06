99d9e3c535ac00e213266c7a7d98e2670f1896af
MMahadShahid <mahad@sheridancollege.ca>
2023-07-06 16:17:53 -0400

    Midterm Version 1

--- src/days/AllDays.java
+++ src/days/AllDays.java
@@ -9,5 +9,20 @@ package days;
  * @author mahad
  */
 public enum AllDays {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY;
+     MONDAY(1),TUESDAY(2),WEDNESDAY(3),THURSDAY(4),FRIDAY(5),SATURDAY(6),SUNDAY(7);
+}
+
+private final int weekday;
+    
+    AllDays(int weekday) {
+        this.weekday = weekday;
+    }
+    
+    public void dayName(int code) {
+        for (AllDays day : AllDays.values()) {
+            if (day.weekday == code) {
+                System.out.println(day);
+            }
+        }
+    }
 }
